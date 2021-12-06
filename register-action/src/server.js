7da46834c484adf398ad15db6fc6574c389f9725
catalinpit <catalinpit@gmail.com>
2021-12-06 10:29:12 +0200

    Initial Commit

---
+++ register-action/src/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+const REGISTRATION = `
+mutation ($name: String!, $username: String!, $email: String!, $password: String!) {
+  insert_users_one(
+    object: {
+      name: $name,
+      username: $username,
+      email: $email,
+      password: $password
+    }
+  ) { id }
+}
+`;
+
+const execute = async (variables, reqHeaders) => {
+  const fetchResponse = await fetch(
+    "https://maximum-quail-14.hasura.app/v1/graphql",
+    {
+      method: 'POST',
+      headers: reqHeaders || {},
+      body: JSON.stringify({
+        query: REGISTRATION,
+        variables
+      })
+    }
+  );
+  const data = await fetchResponse.json();
+  return data;
+};
+  
+
+// Request Handler
+app.post('/register', async (req, res) => {
+
+  // get request input
+  const { name, username, email, password } = req.body.input;
+
+  // encrypt the password
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const { data, errors } = await execute({ name, username, email, password: hashedPassword }, req.headers);
+
+  if (errors) {
+    return res.status(400).json(errors[0])
+  }
+
+  return res.json({
+    ...data.insert_users_one
+  });
+});
+
+app.listen(PORT);
